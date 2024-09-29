69e1e43d7256abe0e82a2d22e2997eec7a7feceb
gn08 <gabbyfilipova@gmail.com>
2024-09-29 15:09:43 -0700

    commit 3:10

--- lab15.cpp
+++ lab15.cpp
@@ -9,15 +9,27 @@ class Movie{
 
     public:
         void set_title(){
-
+            title = t;
         }
 
         void set_year(){
-
+            year = y;
         }
 
         void set_screen_writer(){
-            
+            screen_writer = sw;
         }
+
+        string get_title() const{
+            return title;
+        }
+
+        int get_year() const{
+            return year;
+        }
+
+        string get_screen_writer() const{
+            return screen_writer;
+        } 
 
 }
